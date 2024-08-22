d73a62e500732f11ca390bd47452e7857d40c521
simon <simonwubshet254@gmail.com>
2024-08-22 22:39:58 +0300

    create_products_table

--- example-app/database/migrations/2024_08_07_085430_create_products_table.php
+++ example-app/database/migrations/2024_08_07_085430_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFlightsTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateFlightsTable extends Migration
      */
     public function up()
     {
-        Schema::create('migrations', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('sex');
-            $table->integer('age');
             $table->timestamps();
-
-            
         });
     }
 
@@ -31,6 +27,6 @@ class CreateFlightsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('flights');
+        Schema::dropIfExists('products');
     }
 }
